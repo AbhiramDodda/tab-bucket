aea6f69abbb7d1bca71e835cb82558d0c2910673
AbhiramDodda <abhiramdodda@gmail.com>
2025-04-04 16:30:33 +0530

    solved single link opening, new logo and start of new v1.1

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Tab-Bucket
 ## A browser extension to copy and open multipe links across tabs in a browser.
 ### This is the Chrome version 
+<a href="https://chromewebstore.google.com/detail/mebliglplpfbnljphggmjinbgjlhkgjd?utm_source=item-share-cb"> Add the Extension </a>
--- popup.js
+++ popup.js
@@ -1,105 +1,112 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const urlList = document.getElementById('url-list');
-    const selectAllCheckbox = document.getElementById('select-all');
-    const copyButton = document.getElementById('copy-button');
-    const pasteDetectButton = document.getElementById('paste-detect-button');
-    const message = document.getElementById('message');
-    const newTabUrls = [
-      'chrome://newtab/',
-      'about:newtab',
-      'edge://newtab/',
-      'brave://newtab/',
-      'chrome://startpageshared/',
-      'about:home',
-      'about:blank'
-    ];
-    // Get all tabs and display their URLs
-    chrome.tabs.query({}, function(tabs) {
-      
-      tabs.forEach(function(tab, index) {
-        if (tab.url && !newTabUrls.includes(tab.url) && tab.url !== '' && tab.url.startsWith('http')){
-        const urlItem = document.createElement('div');
-        urlItem.className = 'url-item';
-        
-        const checkbox = document.createElement('input');
-        checkbox.type = 'radio';
-        checkbox.className = 'url-checkbox';
-        checkbox.dataset.url = tab.url;
-        
-        const urlText = document.createElement('span');
-        urlText.className = 'url-text';
-        urlText.textContent = tab.title;
-        urlText.title = tab.url;
-        
-        urlItem.appendChild(checkbox);
-        urlItem.appendChild(urlText);
-        urlList.appendChild(urlItem);
-        }
-      });
-    });
+  const urlList = document.getElementById('url-list');
+  const selectAllCheckbox = document.getElementById('select-all');
+  const copyButton = document.getElementById('copy-button');
+  const pasteDetectButton = document.getElementById('paste-detect-button');
+  const message = document.getElementById('message');
+  const newTabUrls = [
+    'chrome://newtab/',
+    'about:newtab',
+    'edge://newtab/',
+    'brave://newtab/',
+    'chrome://startpageshared/',
+    'about:home',
+    'about:blank'
+  ];
+  // Get all tabs and display their URLs
+  chrome.tabs.query({}, function(tabs) {
     
-    // Select/deselect all URLs
-    selectAllCheckbox.addEventListener('change', function() {
-      const checkboxes = document.getElementsByClassName('url-checkbox');
-      for (let checkbox of checkboxes) {
-        checkbox.checked = selectAllCheckbox.checked;
+    tabs.forEach(function(tab, index) {
+      if (tab.url && !newTabUrls.includes(tab.url) && tab.url !== '' && tab.url.startsWith('http')){
+      const urlItem = document.createElement('div');
+      urlItem.className = 'url-item';
+      
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.className = 'url-checkbox';
+      checkbox.dataset.url = tab.url;
+      
+      const urlText = document.createElement('span');
+      urlText.className = 'url-text';
+      urlText.textContent = tab.title;
+      urlText.title = tab.url;
+      
+      urlItem.appendChild(checkbox);
+      urlItem.appendChild(urlText);
+      urlList.appendChild(urlItem);
       }
     });
+  });
   
-    copyButton.addEventListener('click', function() {
-        const checkboxes = document.getElementsByClassName('url-checkbox');
-        const selectedUrls = [];
-        
-        for (let checkbox of checkboxes) {
-          if (checkbox.checked) {
-            selectedUrls.push(checkbox.dataset.url);
-          }
+  // Select/deselect all URLs
+  selectAllCheckbox.addEventListener('change', function() {
+    const checkboxes = document.getElementsByClassName('url-checkbox');
+    for (let checkbox of checkboxes) {
+      checkbox.checked = selectAllCheckbox.checked;
+    }
+  });
+
+  copyButton.addEventListener('click', function() {
+      const checkboxes = document.getElementsByClassName('url-checkbox');
+      const selectedUrls = [];
+      
+      for (let checkbox of checkboxes) {
+        if (checkbox.checked) {
+          selectedUrls.push(checkbox.dataset.url);
         }
-        
-        if (selectedUrls.length > 0) {
-          const urlString = selectedUrls.join(';');
-          navigator.clipboard.writeText(urlString).then(function() {
-            message.style.display = 'block';
-            message.textContent = 'URLs copied to clipboard!';
-            setTimeout(function() {
-              message.style.display = 'none';
-            }, 2000);
+      }
+      
+      if (selectedUrls.length > 0) {
+        let urlString = '';
+        for (let i = 0; i < selectedUrls.length; i++) {
+          urlString += selectedUrls[i];
+          urlString += ';';
+          
+        }
+
+        navigator.clipboard.writeText(urlString).then(function() {
+          message.style.display = 'block';
+          message.textContent = 'URLs copied to clipboard!';
+          setTimeout(function() {
+            message.style.display = 'none';
+          }, 2000);
+        });
+      } else {
+        message.style.display = 'block';
+        message.textContent = 'No URLs selected!';
+        message.style.color = 'red';
+        setTimeout(function() {
+          message.style.display = 'none';
+          message.style.color = 'green';
+        }, 2000);
+      }
+    });
+    
+    // Detect and open pasted URLs
+    pasteDetectButton.addEventListener('click', function() {
+      navigator.clipboard.readText().then(function(text) {
+        if (text && text.includes(';')) {
+          const urls = text.split(';');
+          message.style.display = 'block';
+          message.textContent = `Opening ${urls.length-1} URLs...`;
+          
+          urls.forEach(function(url) {
+            if (url.trim().startsWith('http')) {
+              chrome.tabs.create({ url: url.trim() });
+            }
           });
+          
+          setTimeout(function() {
+            message.style.display = 'none';
+          }, 2000);
         } else {
           message.style.display = 'block';
-          message.textContent = 'No URLs selected!';
+          message.textContent = 'No valid URL string found in clipboard!';
           message.style.color = 'red';
           setTimeout(function() {
             message.style.display = 'none';
           }, 2000);
         }
       });
-      
-      // Detect and open pasted URLs
-      pasteDetectButton.addEventListener('click', function() {
-        navigator.clipboard.readText().then(function(text) {
-          if (text && text.includes(';')) {
-            const urls = text.split(';');
-            message.style.display = 'block';
-            message.textContent = `Opening ${urls.length} URLs...`;
-            
-            urls.forEach(function(url) {
-              if (url.trim().startsWith('http')) {
-                chrome.tabs.create({ url: url.trim() });
-              }
-            });
-            
-            setTimeout(function() {
-              message.style.display = 'none';
-            }, 2000);
-          } else {
-            message.style.display = 'block';
-            message.textContent = 'No valid URL string found in clipboard!';
-            message.style.color = 'red';
-            setTimeout(function() {
-              message.style.display = 'none';
-            }, 2000);
-          }
-        });
-      });
     });
+  });
